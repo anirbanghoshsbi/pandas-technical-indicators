7317de0ded97831310540166f96d0ea5db446246
Anirban Ghosh <40670707+anirbanghoshsbi@users.noreply.github.com>
2020-08-18 07:48:41 +0530

    Create easy_line_ploting.py

---
+++ easy_line_ploting.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def lineplot(frames_y, figL , figW , df , predictions,frames_x=1,no_of_datapoints=0):
+     '''
+     Draws Line plots stacks them on one another
+     
+     '''
+    plt.style.use('fivethirtyeight')
+    fig, ax = plt.subplots(frames_y,frames_x,figsize=(figL,figW))
+    counter=0
+    while counter<frame_y:
+        ax[counter].plot(df.index.values[-no_of_datapoints:],df.Close.values[-no_of_datapoints:],linewidth=1 , marker='*')
+        counter+=1
+        ax[counter].plot(df.index.values[-no_of_datapoints:],predictions.values[-no_of_datapoints:],linewidth=1, marker='.')
+        counter+=1
+
+    plt.show()
